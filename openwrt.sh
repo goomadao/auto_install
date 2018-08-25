89bc6a0a29e40a1109c3885b1874db254c4dde1d
unknown <goomadao@gmail.com>
2018-08-26 00:46:19 +0800

    添加设置任务计划

--- openwrt.sh
+++ openwrt.sh
@@ -13,9 +13,13 @@ sed -i 's/local arp_table = luci\.sys\.net\.arptable() or {}/local arp_table = l
 #install frp
 cd /tmp
 wget https://github.com/fatedier/frp/releases/download/v0.21.0/frp_0.21.0_linux_mipsle.tar.gz
-tar xzvf frp_0.21.0_linux_mipsle.tar.gz -C /usr
-cd /usr
+tar xzvf frp_0.21.0_linux_mipsle.tar.gz
 mv frp_0.21.0_linux_mipsle frp
+cd frp
+rm -rf *frps*
+cd ..
+mv frp /usr/
+cd /usr
 cd frp
 
 cat > frpc.ini <<EOF
@@ -68,3 +72,22 @@ local_port = 80
 remote_port = 8080
 EOF
 
+
+
+
+#为openwrt添加任务计划
+#包括：
+#1.每分钟检查有没有以配置文件frpc.ini和frpc1运行frpc 若没有就运行
+#2.早上八点重启路由器
+cd /etc/crontabs
+cat > root <<EOF
+*/1 * * * * [ $(ps | grep frpc.ini | grep -v grep | wc -l) -eq 0 ] && cd /usr/frp && ./frpc -c frpc.ini
+*/1 * * * * [ $(ps | grep frpc1 | grep -v grep | wc -l) -eq 0 ] && cd /usr/frp && ./frpc -c frpc1
+0 8 * * * reboot
+
+
+
+
+EOF
+/etc/init.d/cron start
+/etc/init.d/cron enable
